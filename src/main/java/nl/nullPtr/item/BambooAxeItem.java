d7eda7edc2c2420f7b257702cdcca53d34a8e36d
Jeroen_Suurmond <jsuurmond@student.scalda.nl>
2024-02-25 23:10:23 +0100

    Haha vergeten branch op te zetten dus dan nu maar vast. Added: - Flint, Stone, Iron, Gold, Diamond and Netherite bamboo axes - Crafting recipes, textures and models for those items

--- src/main/java/nl/nullPtr/item/BambooAxeItem.java
+++ src/main/java/nl/nullPtr/item/BambooAxeItem.java
@@ -3,8 +3,6 @@ package nl.nullPtr.item;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ToolMaterial;
-import net.minecraft.registry.tag.BlockTags;
-import nl.nullPtr.toolMaterial.BambooToolMaterial;
 
 public class BambooAxeItem extends AxeItem {
     public BambooAxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Item.Settings settings) {
