bed8eaa01e9e41261fa6fffb13252ae88f429642
DESKTOP-L4V3QNJ\jdsuu <238632@student.scalda.nl>
2024-03-11 11:51:25 +0100

    YEET

--- src/main/java/nl/nullPtr/BambooItems.java
+++ src/main/java/nl/nullPtr/BambooItems.java
@@ -44,6 +44,22 @@ public class BambooItems implements ModInitializer {
     private static final PickaxeItem DIAMOND_BAMBOO_PICKAXE = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "diamond_bamboo_pickaxe"), new PickaxeItem(BambooToolMaterial.DIAMOND, pickAttackDamage, pickAttackSpeed, new FabricItemSettings()));
     private static final PickaxeItem NETHERITE_BAMBOO_PICKAXE = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "netherite_bamboo_pickaxe"), new PickaxeItem(BambooToolMaterial.NETHERITE, pickAttackDamage, pickAttackSpeed, new FabricItemSettings()));
 
+    /* Shovels */
+    private static final ShovelItem BAMBOO_SHOVEL = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "bamboo_shovel"), new ShovelItem(BambooToolMaterial.BAMBOO, 3.0f, -3.0f, new FabricItemSettings()));
+    private static final ShovelItem STONE_BAMBOO_SHOVEL = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "stone_bamboo_shovel"), new ShovelItem(BambooToolMaterial.STONE, 3.0f, -3.0f, new FabricItemSettings()));
+    private static final ShovelItem GOLD_BAMBOO_SHOVEL = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "gold_bamboo_shovel"), new ShovelItem(BambooToolMaterial.GOLD, 3.0f, -3.0f, new FabricItemSettings()));
+    private static final ShovelItem IRON_BAMBOO_SHOVEL = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "iron_bamboo_shovel"), new ShovelItem(BambooToolMaterial.IRON, 3.0f, -3.0f, new FabricItemSettings()));
+    private static final ShovelItem DIAMOND_BAMBOO_SHOVEL = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "diamond_bamboo_shovel"), new ShovelItem(BambooToolMaterial.DIAMOND, 3.0f, -3.0f, new FabricItemSettings()));
+    private static final ShovelItem NETHERITE_BAMBOO_SHOVEL = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "netherite_bamboo_shovel"), new ShovelItem(BambooToolMaterial.NETHERITE, 3.0f, -3.0f, new FabricItemSettings()));
+
+    /* Bitches (hoes) */
+    private static final HoeItem BAMBOO_HOE = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "bamboo_hoe"), new HoeItem(BambooToolMaterial.BAMBOO, 0, -3.0f, new FabricItemSettings()));
+    private static final HoeItem STONE_BAMBOO_HOE = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "stone_bamboo_hoe"), new HoeItem(BambooToolMaterial.STONE, 0, -3.0f, new FabricItemSettings()));
+    private static final HoeItem GOLD_BAMBOO_HOE = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "gold_bamboo_hoe"), new HoeItem(BambooToolMaterial.GOLD, 0, -3.0f, new FabricItemSettings()));
+    private static final HoeItem IRON_BAMBOO_HOE = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "iron_bamboo_hoe"), new HoeItem(BambooToolMaterial.IRON, 0, -3.0f, new FabricItemSettings()));
+    private static final HoeItem DIAMOND_BAMBOO_HOE = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "diamond_bamboo_hoe"), new HoeItem(BambooToolMaterial.DIAMOND, 0, -3.0f, new FabricItemSettings()));
+    private static final HoeItem NETHERITE_BAMBOO_HOE = Registry.register(Registries.ITEM, new Identifier("bamboo-items", "netherite_bamboo_hoe"), new HoeItem(BambooToolMaterial.NETHERITE, 0, -3.0f, new FabricItemSettings()));
+
     private static final ItemGroup BAMBOO_TOOLS = FabricItemGroup.builder()
             .icon(() -> new ItemStack(Items.BAMBOO))
             .displayName(Text.translatable("itemGroup.bamboo-items.bamboo_items"))
@@ -51,21 +67,33 @@ public class BambooItems implements ModInitializer {
                 entries.add(BAMBOO_SWORD);
                 entries.add(BAMBOO_PICKAXE);
                 entries.add(BAMBOO_AXE);
+                entries.add(BAMBOO_SHOVEL);
+                entries.add(BAMBOO_HOE);
                 entries.add(STONE_BAMBOO_SWORD);
                 entries.add(STONE_BAMBOO_PICKAXE);
                 entries.add(STONE_BAMBOO_AXE);
-                entries.add(GOLD_BAMBOO_SWORD);
-                entries.add(GOLD_BAMBOO_PICKAXE);
-                entries.add(GOLD_BAMBOO_AXE);
+                entries.add(STONE_BAMBOO_SHOVEL);
+                entries.add(STONE_BAMBOO_HOE);
                 entries.add(IRON_BAMBOO_SWORD);
                 entries.add(IRON_BAMBOO_PICKAXE);
                 entries.add(IRON_BAMBOO_AXE);
+                entries.add(IRON_BAMBOO_SHOVEL);
+                entries.add(IRON_BAMBOO_HOE);
+                entries.add(GOLD_BAMBOO_SWORD);
+                entries.add(GOLD_BAMBOO_PICKAXE);
+                entries.add(GOLD_BAMBOO_AXE);
+                entries.add(GOLD_BAMBOO_SHOVEL);
+                entries.add(GOLD_BAMBOO_HOE);
                 entries.add(DIAMOND_BAMBOO_SWORD);
                 entries.add(DIAMOND_BAMBOO_PICKAXE);
                 entries.add(DIAMOND_BAMBOO_AXE);
+                entries.add(DIAMOND_BAMBOO_SHOVEL);
+                entries.add(DIAMOND_BAMBOO_HOE);
                 entries.add(NETHERITE_BAMBOO_SWORD);
                 entries.add(NETHERITE_BAMBOO_PICKAXE);
                 entries.add(NETHERITE_BAMBOO_AXE);
+                entries.add(NETHERITE_BAMBOO_SHOVEL);
+                entries.add(NETHERITE_BAMBOO_HOE);
             })
             .build();
 
